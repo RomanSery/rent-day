fe4f3540c4f4647c9eb83fcc4c6e45018f25a7d7
Roman Sery <roman.sery@novelution.com>
2021-02-28 15:14:00 -0500

    dokku test

--- src/server/app.ts
+++ src/server/app.ts
@@ -39,7 +39,7 @@ mongoose
 */
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set("port", 8081); //process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
