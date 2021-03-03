fe05548893db6cb86e656ba5af2b475b21049b14
Roman Sery <roman.sery@novelution.com>
2021-03-03 18:36:13 -0500

    dokku test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,12 +5,7 @@ module.exports = function(app) {
     '/api',
     createProxyMiddleware({
       target: "https://rentday.coderdreams.com",
-      changeOrigin: true,
-      ws: true,
-      router: {
-        // when request.headers.host == 'dev.localhost:3000',
-        "localhost:3000/api": "http://localhost:4000",
-      },
+      changeOrigin: true,      
     })
   );
 };
