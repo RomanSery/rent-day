30e729ab86e87eeccc72c0aad8fdfc86f5a5ee8a
Roman Sery <44503387+RomanSery@users.noreply.github.com>
2021-08-07 10:58:29 -0400

    point to external db server

--- src/server/util/secrets.ts
+++ src/server/util/secrets.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import logger from "./logger";
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 
 logger.debug("environment: " + process.env.NODE_ENV);
 
@@ -15,11 +15,6 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   
 }*/
 
-logger.debug("Using .env.production file to supply config environment variables");
-  dotenv.config({
-    path: "src/server/.env.production",
-    debug: true,
-  });
 
 export const DB_CONN_STR: string = process.env.DB_CONN_STR!;
 export const IS_DEV: boolean =
